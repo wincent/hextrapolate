347f14f2d256bd17c55d17757c2d90d7694d4e03
Wincent Colaiuta <win@wincent.com>
2009-12-27 01:56:32 +0100

    Add "GMP" target
    This target runs a script that does a new, clean build of the GMP
    library.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

---
+++ gmp.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# set up SRCROOT so we can run outside of Xcode if need be
+if [ -z "$SRCROOT" ]; then
+  SRCROOT=.
+fi
+
+# get platform; eg: pentiumm-apple-darwin10.0.0
+GUESS=$(cd $SRCROOT/gmp && ./config.guess)
+
+# extract CPU part; eg: pentiumm
+CPU=$(echo $GUESS | awk -F- '{ print $1 }')
+
+# produce final build string; eg: none-apple-darwin10.0.0
+BUILDSTRING=${GUESS/$CPU/none}
+
+# now perform the actual build
+cd ${SRCROOT}/gmp && \
+   make clean && \
+   ./configure --disable-shared --build=$BUILDSTRING && \
+   make
