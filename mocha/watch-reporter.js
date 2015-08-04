0aa96a0cae417efaa5e349353f6e3096412f89c8
Greg Hurrell <greg@hurrell.net>
2015-08-04 00:05:35 -0700

    Update copyright year ranges
    Just to show how old this frickin' thing is.

--- mocha/watch-reporter.js
+++ mocha/watch-reporter.js
@@ -1,5 +1,5 @@
 /**
- * Copyright 2015-present Greg Hurrell. All rights reserved.
+ * Copyright 2003-present Greg Hurrell. All rights reserved.
  * Licensed under the terms of the MIT license.
  */
 
