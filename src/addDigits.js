9abb68baab08cb6b636c4a7b782b97e72694a30e
Greg Hurrell <greg@hurrell.net>
2017-06-02 08:37:20 -0700

    Microoptimization
    Which may or may not be perceptible, and I can't pick it up in the
    profiler because it is too noisy.

--- src/addDigits.js
+++ src/addDigits.js
@@ -16,9 +16,15 @@ export default function addDigits(
 ): Array<number> {
   let result = [];
   let carry = 0;
-  for (let i = 0; i < aDigits.length || i < bDigits.length || carry; i++) {
-    const aDigit = i < aDigits.length ? aDigits[i] : 0;
-    const bDigit = i < bDigits.length ? bDigits[i] : 0;
+  let aLength = aDigits.length;
+  let bLength = bDigits.length;
+  for (
+    let i = 0, max = Math.max(aLength, bLength);
+    carry || i < max;
+    i++
+  ) {
+    const aDigit = i < aLength ? aDigits[i] : 0;
+    const bDigit = i < bLength ? bDigits[i] : 0;
     const sum = aDigit + bDigit + carry;
     result.push(sum % base);
 
