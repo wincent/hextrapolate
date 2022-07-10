78f25bb23995b2f382a778fb77a614b6ff0db71f
Greg Hurrell <greg@hurrell.net>
2022-07-10 22:39:11 +0200

    refactor: put things back the way they were
    As mentioned in parent commit.

--- src/Size.react.js
+++ src/Size.react.js
@@ -9,15 +9,7 @@ import React from 'react';
 import convert from './convert';
 
 export default function Size({value}) {
-  let roundup = 1;
-  let targetBase = 2;
-  if (value.base === 16) {
-    roundup = 4;
-    targetBase = 16;
-  }
-  const bits = value ?
-    convert(value.value, value.base, targetBase).length * roundup :
-    0;
+  const bits = value ? convert(value.value, value.base, 2).length: 0;
   const bytes = Math.ceil(bits / 8);
   return (
     <span>
