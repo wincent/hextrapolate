933b344ba1012a9378a27335eb71420bbf71e457
Greg Hurrell <greg@hurrell.net>
2022-07-10 22:35:10 +0200

    feat: special-case base 16 size calculations
    Slight refinement to what I did back in e4f88cd.
    
    When typing in any input field, `value.base` will reflect the base of
    that field. For almost all bases, the behavior implemented in e4f88cd is
    what you want.
    
    But when you're typing in the hex field people are much more accustomed
    to thinking in "nibbes" (ie. half-bytes). You don't want to type "1" and
    see it be a 1-bit number; you want it to jump straight to 4 bits.
    
    Or maybe not. I'm 50:50 on this and will probably revert it back, while
    keeping the component functional.
    
    Bonus: converted to functional component.

--- src/Size.react.js
+++ src/Size.react.js
@@ -6,24 +6,24 @@
  */
 
 import React from 'react';
-import {ValuePropType} from './Field.react';
 import convert from './convert';
 
-export default class Size extends React.Component {
-  static propTypes = {
-    value: ValuePropType,
-  };
-
-  render() {
-    const {value} = this.props;
-    const bits = value ? convert(value.value, value.base, 2).length : 0;
-    const bytes = Math.ceil(bits / 8);
-    return (
-      <span>
-        {bytes} byte{bytes === 1 ? '' : 's'},
-        {' '}
-        {bits} bits
-      </span>
-    );
+export default function Size({value}) {
+  let roundup = 1;
+  let targetBase = 2;
+  if (value.base === 16) {
+    roundup = 4;
+    targetBase = 16;
   }
+  const bits = value ?
+    convert(value.value, value.base, targetBase).length * roundup :
+    0;
+  const bytes = Math.ceil(bits / 8);
+  return (
+    <span>
+      {bytes} byte{bytes === 1 ? '' : 's'},
+      {' '}
+      {bits} bits
+    </span>
+  );
 }
